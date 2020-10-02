84615fb58c560c330bfbb26fe922108ebd7ae72f
DESKTOP-JRTNCR8\lenus <harchevnikovdenis@mail.ru>
2020-10-02 16:21:15 +0300

    Реализовал возможность выбора цветов

---
+++ Assets/Scripts/ColorPond.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPond : MonoBehaviour
+{
+    [SerializeField] ColorSelector colorSelector;
+    [SerializeField] private Color colorInThisPond;
+
+    private void OnMouseEnter() 
+    {
+        colorSelector.SetNewColor(colorInThisPond);
+    }
+}
---
+++ Assets/Scripts/ColorSelector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PaintIn3D;
+
+public class ColorSelector : MonoBehaviour
+{
+    [SerializeField] private P3dPaintSphere painter;
+    public Color currentColor {private get; set;}
+
+    public void SetNewColor(Color color)
+    {
+        painter.Color = color;
+    }
+}
